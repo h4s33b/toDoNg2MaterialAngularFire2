d85113ba8f773da4ee591248b5de9b9b67c2e123
h4s33b <haseeb.rehman@iunc.edu.pk>
2016-11-07 21:53:10 +0500

    Updated code for update data function and added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,30 @@ import { Todo } from './Todo';
 })
 export class AppComponent {
   title = 'app works!';
+  showFormData = false;
   currentDate = Date.now();
   items: FirebaseListObservable<any[]>;
-  model = new Todo("", "", "Medium");
+  item1 : FirebaseListObservable<any>;
+  public model = new Todo("", "", "Medium");
   constructor(public af: AngularFire) {
+    //this.items = af.database.list('/items', { preserveSnapshot: true });
     this.items = af.database.list('/items');
   }
 
   writeUserData() {
-     console.log("form submited:" + JSON.stringify(this.model));
-     if(this.model && this.model.title && this.model.details && this.model.priority){
-       this.items.push(this.model);
-       this.model = {"title":"","details":"","priority":"Medium"};
-     }else{
-       alert("Please Enter All Fields.");
-     }
+    console.log("form submited:" + JSON.stringify(this.model));
+    if (this.model && this.model.title && this.model.details && this.model.priority) {
+      this.items.push(this.model);
+      this.model = { "title": "", "details": "", "priority": "Medium" };
+    } else {
+      alert("Please Enter All Fields.");
+    }
+  }
+
+  updateData(todoData) {
+    // this.item1 = this.af.database.list('/items/'+i);
+    // this.item1 = this.items[i];
+     this.model = { "title": todoData.title, "details": todoData.details, "priority": todoData.priority };
+    // this.model = this.af.database.list('/items').child(i);
   }
 }
